d561cd1a76bc9538c9067ae5a79c284319d32d42
mikumifa <99951454+mikumifa@users.noreply.github.com>
2022-12-18 15:15:43 +0800

    Update loadFile.sh

--- loadFile.sh
+++ loadFile.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 git clone https://gitee.com/mikumifa/QChatGPT
 mv QChatGPT bot
-cp -f requirements.txt bot/
 wget -O mcl.zip https://github.com/iTXTech/mirai-console-loader/releases/download/v2.1.2/mcl-2.1.2.zip
 unzip -o -d  ./mirai mcl.zip 
 rm mcl.zip
