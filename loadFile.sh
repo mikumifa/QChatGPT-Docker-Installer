2e33e282bf69a71f4596b0afd45549c8bbda94d0
mikumifa <99951454+mikumifa@users.noreply.github.com>
2023-02-11 16:45:04 +0800

    Delete loadFile.sh

--- loadFile.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-git clone https://github.com/RockChinQ/QChatGPT
-mv QChatGPT bot
-wget -O mcl.zip https://github.com/iTXTech/mirai-console-loader/releases/download/v2.1.2/mcl-2.1.2.zip
-unzip -o -d  ./mirai mcl.zip 
-rm mcl.zip
